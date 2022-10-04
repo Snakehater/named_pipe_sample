65ddbe3851fdc0ccfc38df1b472b88c092c0ea78
Snakehater <vigor.turujlija@gmail.com>
2022-10-04 14:07:51 +0200

    Added fifo logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
-#include <stdio.h>
+#include <stdio.h> // standard input output
+#include <stdlib.h>
+#include <sys/stat.h> // mkfifo
+#include <unistd.h>
+#include <string.h>
+#include <linux/stat.h>
 
 int main(int argc, char** argv)
 {
-	printf("Hello world\n");
+	FILE *fp;
+	const char * fifo = "/tmp/named_pipe";
+	mkfifo(fifo, 0666);
+
+	char buf[10];
+
+	fp = fopen(fifo, "r");
+	fgets(buf, 10, fp);
+	printf("Received string: %s\n", buf);
+	fclose(fp);
+
 	return 0;
 }
